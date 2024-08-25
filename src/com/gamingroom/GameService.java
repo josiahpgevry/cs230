39f669bb87c12ef2a2ad04980fe7bbe6dfb4a01d
josiahpgevry <137459168+josiahpgevry@users.noreply.github.com>
2024-08-25 14:20:42 -0700

    Add files via upload

---
+++ src/com/gamingroom/GameService.java
@@ -0,0 +1,127 @@
+package com.gamingroom;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+
+//Josiah Gevry
+
+/**
+ * A singleton service for the game engine
+ * 
+ * @author coce@snhu.edu
+ */
+public class GameService {
+
+	/**
+	 * A list of the active games
+	 */
+	private static List<Game> games = new ArrayList<Game>();
+
+	/*
+	 * Holds the next game identifier
+	 */
+	private static long nextGameId = 1;
+
+	// FIXME: Add missing pieces to turn this class a singleton 
+	// Added GameService singleton
+	private static GameService instance = new GameService();
+	
+	private GameService() {}
+	
+	public static GameService getInstance() {
+		return instance;
+	}
+	
+	/**
+	 * Construct a new game instance
+	 * 
+	 * @param name the unique name of the game
+	 * @return the game instance (new or existing)
+	 */
+	
+	public Game addGame(String name) {
+
+		// a local game instance
+		Game game = null;
+
+		// FIXME: Use iterator to look for existing game with same name
+		// if found, simply return the existing instance
+		
+		//created new iterator for games arraylist 
+		Iterator<Game> newGame = games.iterator();
+		
+		//created if statement for when array is not empty to return game if already on list
+		
+		if (game != null) {
+			if (game.equals(name)) {
+				return game;
+			}
+		}
+
+		// if not found, make a new game instance and add to list of games
+		if (game == null) {
+			game = new Game(nextGameId++, name);
+			games.add(game);
+		}
+		
+		// return the new/existing game instance to the caller
+		return game;
+	}
+
+	/**
+	 * Returns the game instance at the specified index.
+	 * <p>
+	 * Scope is package/local for testing purposes.
+	 * </p>
+	 * @param index index position in the list to return
+	 * @return requested game instance
+	 */
+	Game getGame(int index) {
+		return games.get(index);
+	}
+	
+	/**
+	 * Returns the game instance with the specified id.
+	 * 
+	 * @param id unique identifier of game to search for
+	 * @return requested game instance
+	 */
+	public Game getGame(long id) {
+
+		// a local game instance
+		Game game = null;
+
+		// FIXME: Use iterator to look for existing game with same id
+		// if found, simply assign that instance to the local variable
+
+		return game;
+	}
+
+	/**
+	 * Returns the game instance with the specified name.
+	 * 
+	 * @param name unique name of game to search for
+	 * @return requested game instance
+	 */
+	public Game getGame(String name) {
+
+		// a local game instance
+		Game game = null;
+
+		// FIXME: Use iterator to look for existing game with same name
+		// if found, simply assign that instance to the local variable
+
+		return game;
+	}
+
+	/**
+	 * Returns the number of games currently active
+	 * 
+	 * @return the number of games currently active
+	 */
+	public int getGameCount() {
+		return games.size();
+	}
+}
---
+++ src/com/gamingroom/ProgramDriver.java
@@ -0,0 +1,38 @@
+package com.gamingroom;
+
+//Josiah Gevry
+
+/**
+ * Application start-up program
+ * 
+ * @author coce@snhu.edu
+ */
+public class ProgramDriver {
+	
+	/**
+	 * The one-and-only main() method
+	 * 
+	 * @param args command line arguments
+	 */
+	public static void main(String[] args) {
+		
+		// FIXME: obtain reference to the singleton instance
+		// changed null to reference singleton "GameService"
+		
+		GameService service = GameService.getInstance(); // replace null with ???
+		
+		System.out.println("\nAbout to test initializing game data...");
+		
+		// initialize with some game data
+		Game game1 = service.addGame("Game #1");
+		System.out.println(game1);
+		Game game2 = service.addGame("Game #2");
+		System.out.println(game2);
+		
+		
+		
+		// use another class to prove there is only one instance
+		SingletonTester tester = new SingletonTester();
+		tester.testSingleton();
+	}
+}
